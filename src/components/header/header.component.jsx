1309b2421fdd3bd7d93a8883f706a57839064a65
thsyd1982 <salah.yacine.douakha@gmail.com>
2020-07-03 14:22:48 +0200

    test ngix

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../navbar/navbar.component';
+import Hero from '../hero/hero.component';
+
+const Header = () => {
+    return ( 
+    <div className="header"> 
+    <Navbar/>
+    
+    </div> 
+    );
+}
+ 
+export default Header;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from '../../components/hero/hero.component';
+const Home = () => {
+    return (<div className="home">
+        <Hero title='Home'/>
+        <div className="container">
+        
+        </div>
+    </div>  );
+}
+ 
+export default Home;
